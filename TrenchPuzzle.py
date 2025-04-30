155b621acbe5f29969809509e94eac0295d6161c
SRaionido <sleon027@ucr.edu>
2025-04-30 09:33:05 -0700

    Adding prototypes of operators

--- TrenchPuzzle.py
+++ TrenchPuzzle.py
@@ -2,3 +2,21 @@ testVar = 2
 
 def testFunct(initial_state):
     print("Initial state: ", initial_state)
+
+
+def blank_down(state, index):
+    # Move the blank down if possible
+    pass
+
+def blank_up(state, index):
+    # Move the blank up if possible
+    pass
+
+def blank_left(state, index):
+    # Move the blank left if possible
+    pass
+
+def blank_right(state, index):
+    # Move the blank right if possible
+    pass
+
